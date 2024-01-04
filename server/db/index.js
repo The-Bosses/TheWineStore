4e8156a977d412a787e2cb7ac231bf58f0484482
Benjamin <benjamin.boss@icloud.com>
2024-01-04 02:22:11 -0500

    updated data base

--- server/db/index.js
+++ server/db/index.js
@@ -33,13 +33,30 @@ const seed = async()=> {
       created_at TIMESTAMP DEFAULT now(),
       username VARCHAR(100) UNIQUE NOT NULL,
       password VARCHAR(100) NOT NULL,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL,
+      birth_date VARCHAR(50) NOT NULL,
+      address_1 VARCHAR(120) NOT NULL,
+      address_2 VARCHAR(120),
+      city VARCHAR(100) NOT NULL,
+      state VARCHAR(50) NOT NULL,
+      country VARCHAR(50) NOT NULL,
+      postal_code VARCHAR(16) NOT NULL,
+      is_vip BOOLEAN DEFAULT false NOT NULL,
       is_admin BOOLEAN DEFAULT false NOT NULL
     );
 
     CREATE TABLE products(
       id UUID PRIMARY KEY,
       created_at TIMESTAMP DEFAULT now(),
-      name VARCHAR(100) UNIQUE NOT NULL
+      name VARCHAR(100) UNIQUE NOT NULL,
+      type VARCHAR(50) NOT NULL,
+      location VARCHAR(50),
+      alcohol_percent INTEGER, 
+      description TEXT,
+      price INTEGER NOT NULL,
+      reviews TEXT,
+      is_vip BOOLEAN DEFAULT false NOT NULL
     );
 
     CREATE TABLE orders(
@@ -61,16 +78,14 @@ const seed = async()=> {
   `;
   await client.query(SQL);
 
-  const [moe, lucy, ethyl] = await Promise.all([
-    createUser({ username: 'moe', password: 'm_password', is_admin: false}),
-    createUser({ username: 'lucy', password: 'l_password', is_admin: false}),
-    createUser({ username: 'ethyl', password: '1234', is_admin: true})
+  const [ben, ethyl] = await Promise.all([
+    createUser({ username: 'Ben', password: 'bboss', name: 'Ben Boss', email: 'bb@icloud.com', birth_date:'1998-03-09', address_1: '123 Ypsilanti Dr', address_2: '', city: 'Ypsilanti', state: 'Michigan', country: 'United States', postal_code: '48197', is_vip: true, is_admin: true }),
+    createUser({ username: 'ethyl', password: '1234', name: 'Ethyl', email: 'e@me.com', birth_date: '1900-01-01', address_1: '234 Lane', address_2:'', city: 'Detroit', state: 'Michigan', country: "United States", postal_code: '12345', is_vip: true, is_admin: true})
   ]);
-  const [foo, bar, bazz] = await Promise.all([
-    createProduct({ name: 'foo' }),
-    createProduct({ name: 'bar' }),
-    createProduct({ name: 'bazz' }),
-    createProduct({ name: 'quq' }),
+  const [bar, foo] = await Promise.all([
+    createProduct({ name: 'bar', type: 'Merlot', location: 'Napa Valley', alcohol_percent: 10, description: 'yummy in my tummy', price: 9, reviews: 'incredible', is_vip: false }),
+    createProduct({ name: 'foo', type: 'Merlot', location: 'Napa Valley', alcohol_percent: 10, description: 'yummy in my tummy', price: 9, reviews: 'incredible', is_vip: false })
+
   ]);
   let orders = await fetchOrders(ethyl.id);
   let cart = orders.find(order => order.is_cart);
