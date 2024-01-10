3b20a2521fd79052f9e0a5b0db7f8f33357d0db3
Sam4377 <tenz4377@gmail.com>
2024-01-10 11:57:47 -0500

    added the users for everyone

--- server/db/index.js
+++ server/db/index.js
@@ -52,9 +52,9 @@ const seed = async()=> {
       name VARCHAR(100) UNIQUE NOT NULL,
       type VARCHAR(50) NOT NULL,
       location VARCHAR(50),
-      alcohol_percent INTEGER, 
+      alcohol_percent DECIMAL(3,1), 
       description TEXT,
-      price INTEGER NOT NULL,
+      price DECIMAL(4,2),
       reviews TEXT,
       is_vip BOOLEAN DEFAULT false NOT NULL
     );
@@ -78,18 +78,22 @@ const seed = async()=> {
   `;
   await client.query(SQL);
 
-  const [ben, ethyl] = await Promise.all([
-    createUser({ username: 'Ben', password: 'bboss', name: 'Ben Boss', email: 'bb@icloud.com', birth_date:'1998-03-09', address_1: '123 Ypsilanti Dr', address_2: '', city: 'Ypsilanti', state: 'Michigan', country: 'United States', postal_code: '48197', is_vip: true, is_admin: true }),
-    createUser({ username: 'ethyl', password: '1234', name: 'Ethyl', email: 'e@me.com', birth_date: '1900-01-01', address_1: '234 Lane', address_2:'', city: 'Detroit', state: 'Michigan', country: "United States", postal_code: '12345', is_vip: true, is_admin: true})
+  const [ben, parker, sam, robert] = await Promise.all([
+    createUser({ username: 'ben', password: 'bboss', name: 'Ben Boss', email: 'bb@icloud.com', birth_date:'1998-03-09', address_1: '123 Ypsilanti Dr', address_2: '', city: 'Ypsilanti', state: 'Michigan', country: 'United States', postal_code: '48197', is_vip: true, is_admin: true }),
+    createUser({ username: 'parker', password: '1234', name: 'Parker', email: 'e@me.com', birth_date: '1900-01-01', address_1: '234 Lane', address_2:'', city: 'Detroit', state: 'Michigan', country: "United States", postal_code: '12345', is_vip: true, is_admin: true}),
+    createUser({ username: 'sam', password: '1234', name: 'Sam', email: 'e@me.com', birth_date: '1900-01-01', address_1: '234 Lane', address_2:'', city: 'Detroit', state: 'Michigan', country: "United States", postal_code: '12345', is_vip: true, is_admin: true}),
+    createUser({ username: 'robert', password: '1234', name: 'Robert', email: 'e@me.com', birth_date: '1900-01-01', address_1: '234 Lane', address_2:'', city: 'Detroit', state: 'Michigan', country: "United States", postal_code: '12345', is_vip: true, is_admin: true})
+    
   ]);
-  const [bar, foo] = await Promise.all([
-    createProduct({ name: 'bar', type: 'Merlot', location: 'Napa Valley', alcohol_percent: 10, description: 'yummy in my tummy', price: 9, reviews: 'incredible', is_vip: false }),
-    createProduct({ name: 'foo', type: 'Merlot', location: 'Napa Valley', alcohol_percent: 10, description: 'yummy in my tummy', price: 9, reviews: 'incredible', is_vip: false })
-
+  const [oh_schist, enchanted_garden, foo, bar] = await Promise.all([
+    createProduct({ name: 'Oh Schist', type: 'Riesling', location: 'Germany', alcohol_percent: 9.5, description: 'Overflowing with lively citrus aromas and a tantalizing hint of white florals. Its zesty acidity intertwines seamlessly with juicy peach and tangy lemon notes, balanced by a subtle sweetness. This White Riesling is a vibrant, versatile delight that harmonizes beautifully with any occasion. It pairs well with Asian food.', price: 8.99, reviews: '', is_vip: false }),
+    createProduct({ name: 'Echanted Garden of the Eden Valley', type: 'Riesling', location: 'Australia', alcohol_percent: 11.8, description: 'The “Enchanted Garden” was planted in 1910 and thrives to this day, a seven-acre Eden Valley Vine Garden lovingly tended by Sue and Stuart Woodman. The “La Niña” year in 2022 brought cooler, more mild temperatures, particularly at night, which helped to retain acidity since the respiration of acids occurs to a greater degree when temperatures are warmer. The main notes are honetsuckle, sherbet, and citrus. Pairs very well with deep fruit flavors.', price: 21.99, reviews: '', is_vip: false }),
+    createProduct({ name: 'foo', type: 'Riesling', location: 't', alcohol_percent: 3, description: 'y', price: 9, reviews: 'y', is_vip: false }),
+    createProduct({ name: 'bar', type: 'Riesling', location: 't', alcohol_percent: 3, description: 'y', price: 9, reviews: 'y', is_vip: false }),
   ]);
-  let orders = await fetchOrders(ethyl.id);
+  let orders = await fetchOrders(parker.id);
   let cart = orders.find(order => order.is_cart);
-  let lineItem = await createLineItem({ order_id: cart.id, product_id: foo.id});
+  let lineItem = await createLineItem({ order_id: cart.id, product_id: oh_schist.id});
   lineItem.quantity++;
   await updateLineItem(lineItem);
   lineItem = await createLineItem({ order_id: cart.id, product_id: bar.id});
