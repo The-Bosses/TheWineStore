c305c9ff77f81f6ff401970125c9856f8f9e2345
parkermunson <parkermunson@gmail.com>
2024-01-10 14:17:44 -0500

    A user can now see an error message if they try to log in with invalid credentials.

--- src/Cart.js
+++ src/Cart.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products })=> {
-const totalCost = sumCart()
+const Cart = ({ updateOrder, removeFromCart, lineItems, cart, products }) => {
+  const totalCost = sumCart();
 
-  function sumCart () {
+  function sumCart() {
     let sum = 0;
     {
-      lineItems.filter((lineItem) => {return lineItem.order_id === cart.id}).map(lineItem => {
-        const product = products.find(product => product.id === lineItem.product_id) || {};
-        return (
-          sum += product.price * lineItem.quantity
-        )
-      })
+      lineItems
+        .filter((lineItem) => {
+          return lineItem.order_id === cart.id;
+        })
+        .map((lineItem) => {
+          const product =
+            products.find((product) => product.id === lineItem.product_id) ||
+            {};
+          return (sum += product.price * lineItem.quantity);
+        });
     }
     return sum;
   }
@@ -19,30 +23,34 @@ const totalCost = sumCart()
     <div>
       <h2>Cart</h2>
       <ul>
-        {
-          lineItems.filter(lineItem=> lineItem.order_id === cart.id).map( lineItem => {
-            const product = products.find(product => product.id === lineItem.product_id) || {};
+        {lineItems
+          .filter((lineItem) => lineItem.order_id === cart.id)
+          .map((lineItem) => {
+            const product =
+              products.find((product) => product.id === lineItem.product_id) ||
+              {};
             return (
-              <li key={ lineItem.id }>
-                { product.name }
-                ({ lineItem.quantity })
-                <button onClick={ ()=> removeFromCart(lineItem)}>Remove From Cart</button>
+              <li key={lineItem.id}>
+                {product.name}({lineItem.quantity})
+                <button onClick={() => removeFromCart(lineItem)}>
+                  Remove From Cart
+                </button>
               </li>
             );
-          })
-        }
+          })}
       </ul>
-      {
-        lineItems.filter(lineItem => lineItem.order_id === cart.id ).length ? 
+      {lineItems.filter((lineItem) => lineItem.order_id === cart.id).length ? (
         <div>
-          <div>Total Cost: ${(totalCost.toFixed(2))}</div>
-            <button onClick={()=> {
-            updateOrder({...cart, is_cart: false });
-            }}>Create Order</button>
+          <div>Total Cost: ${totalCost.toFixed(2)}</div>
+          <button
+            onClick={() => {
+              updateOrder({ ...cart, is_cart: false });
+            }}
+          >
+            Create Order
+          </button>
         </div>
-        :
-        null
-      }
+      ) : null}
     </div>
   );
 };
