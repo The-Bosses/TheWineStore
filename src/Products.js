42deca94432e352cd02b19fcd752c60ff4c49b27
Sam4377 <149510131+Sam4377@users.noreply.github.com>
2024-01-23 21:03:31 -0500

    Update Products.js

--- src/Products.js
+++ src/Products.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Products = ({ products, cartItems, createLineItem, updateLineItem, auth})=> {
+  const isVipUser = auth && auth.is_vip
+  
   return (
     <div>
       <h2>Available Wine List</h2>
@@ -9,6 +11,10 @@ const Products = ({ products, cartItems, createLineItem, updateLineItem, auth})=
         {
           products.map( product => {
             const cartItem = cartItems.find(lineItem => lineItem.product_id === product.id);
+            if (product.is_vip && !isVipUser) {
+              return null;
+            }
+            
             return (
               <li key={ product.id }>
                 <Link to={`/product/${product.id.toString()}`}>{ product.name }</Link>
