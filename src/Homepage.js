a06cecc42a508f64c8b2c3d942c7e688428abd93
parkermunson <parkermunson@gmail.com>
2024-01-27 12:44:36 -0500

    added autoplay to slider

--- src/Homepage.js
+++ src/Homepage.js
@@ -20,6 +20,10 @@ const Homepage = ({
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
     afterChange: (index) => setCurrentSlide(index),
     appendDots: (dots) => (
       <div style={{ position: "relative" }}>
