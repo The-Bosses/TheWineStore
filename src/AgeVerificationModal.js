ddc0427e9c47e6a20fda2474d3d576ab65584f5a
parkermunson <parkermunson@gmail.com>
2024-01-27 13:27:44 -0500

    Edited and styled Age Verification Page

--- src/AgeVerificationModal.js
+++ src/AgeVerificationModal.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 const AgeVerificationModal = ({ onClose, onVerify }) => {
-  const [enteredAge, setEnteredAge] = useState("");
   const [verificationSuccessful, setVerificationSuccessful] = useState(false);
 
   useEffect(() => {
@@ -10,30 +9,50 @@ const AgeVerificationModal = ({ onClose, onVerify }) => {
     }
   }, [verificationSuccessful, onClose]);
 
-  const handleVerify = () => {
-    // Validate the entered age
-    if (enteredAge >= 21) {
+  const handleVerify = (isVerified) => {
+    if (isVerified) {
       onVerify();
       setVerificationSuccessful(true);
-      onClose();
     } else {
       alert("You must be at least 21 years old.");
     }
   };
 
   return (
-    <div className="modal">
-      <h2>Age Verification</h2>
-      <p>Please enter your age:</p>
-      <input
-        type="number"
-        value={enteredAge}
-        onChange={(e) => setEnteredAge(e.target.value)}
-      />
-      <button onClick={handleVerify}>Verify</button>
-      <button onClick={onClose} disabled={!verificationSuccessful}>
-        Close
-      </button>
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+      <div className="bg-white text-black p-8 rounded-md">
+        <div className="bg-white max-w-xs overflow-hidden flex items-center justify-center">
+          <img
+            src="public/Logos/plonk_logo_transparent.png"
+            alt="Logo"
+            className="w-32 h-32 object-contain"
+          />
+        </div>
+        <div className="bg-white max-w-xs overflow-hidden flex itmes-center justify-center">
+          <h2 className="text-2xl font-bold mb-4">
+            Sorry, but we have to ask...
+          </h2>
+        </div>
+        <div className="bg-white max-w-xs overflow-hidden flex itmes-center justify-center">
+          <p className="mb-4">Are you at least 21 years old?</p>
+        </div>
+
+        <div></div>
+        <div className="flex items-center justify-center space-x-4">
+          <button
+            onClick={() => handleVerify(true)}
+            className="bg-gray-500 text-white py-2 px-4 rounded-md"
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => handleVerify(false)}
+            className="bg-gray-300 text-white py-2 px-4 rounded-md"
+          >
+            No
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
